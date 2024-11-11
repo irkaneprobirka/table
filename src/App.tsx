ee33f9d912b9e7ef1d3628970473bed29562b45d
irkaneprobirka <liptonhwange@gmail.com>
2024-11-11 09:43:37 +0300

    create page with all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+// App.tsx
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import ColumnSelector from './components/ColumnSelector';
+import CountriesTable from './components/CountriesTable';
+import { getCountries } from './store/countriesStore/countriesApi';
+import { AppDispatch } from './store';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
 
+  useEffect(() => {
+    dispatch(getCountries());
+  }, [dispatch]);
 
-  return <div>hello</div>;
+  return (
+    <div className="container">
+      <ColumnSelector />
+      <CountriesTable />
+    </div>
+  );
 };
 
 export default App;
