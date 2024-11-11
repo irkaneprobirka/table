906b0f194cb47d4f6dba5abe8721ee63cf7915aa
irkaneprobirka <liptonhwange@gmail.com>
2024-11-11 09:34:48 +0300

    create types for project and add toggle look columns reducer in the store

--- src/store/countriesStore/countriesApi.ts
+++ src/store/countriesStore/countriesApi.ts
@@ -1,10 +1,13 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { Country } from "../../types/types";
-import axios from "axios";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { Country } from '../../types/types';
+import axios from 'axios';
 
-
-export const getCountries = createAsyncThunk<Country[]>('countries/getCountries', async () => {
-    const response = await axios.get<Country[]>('https://restcountries.com/v3.1/all');
-    return response.data.slice(0, 50); 
-  });
-  
+export const getCountries = createAsyncThunk<Country[]>(
+  'countries/getCountries',
+  async () => {
+    const response = await axios.get<Country[]>(
+      'https://restcountries.com/v3.1/all',
+    );
+    return response.data.slice(0, 50);
+  },
+);
--- src/store/countriesStore/countriesSlice.ts
+++ src/store/countriesStore/countriesSlice.ts
@@ -1,35 +1,54 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { CountriesState } from "../../types/types";
-import { getCountries } from "./countriesApi";
+import { createSlice } from '@reduxjs/toolkit';
+import { CountriesState } from '../../types/types';
+import { getCountries } from './countriesApi';
 
-const initialState : CountriesState = {
-    coutries : [],
-    loading : false,
-    error : null,
-    activeColumns : ['name', 'languages', 'region', 'population', 'status', 'startOfWeek']
-}
+const initialState: CountriesState = {
+  countries: [],
+  loading: false,
+  error: null,
+  activeColumns: [
+    'name',
+    'languages',
+    'region',
+    'population',
+    'status',
+    'startOfWeek',
+  ],
+};
 
 const countriesSlice = createSlice({
-    name : 'countries',
-    initialState,
-    reducers : {
+  name: 'countries',
+  initialState,
+  reducers: {
+    toggleColumn: (state, action) => {
+      const column = action.payload;
+      if (state.activeColumns.includes(column)) {
+        state.activeColumns = state.activeColumns.filter(
+          (col) => col !== column,
+        );
+      } else {
+        state.activeColumns.push(column);
+      }
     },
-    extraReducers : (builder) => {
-        builder
-        .addCase(getCountries.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        .addCase(getCountries.fulfilled, (state, action) => {
-            state.loading = false;
-            state.error = null;
-            state.coutries = action.payload;
-        })
-        .addCase(getCountries.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.error.message || 'error';
-        })
-    }
-})
-
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCountries.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getCountries.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.countries = action.payload;
+        console.log(state.countries);
+      })
+      .addCase(getCountries.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || 'error';
+        console.log(state.error);
+      });
+  },
+});
+export const { toggleColumn } = countriesSlice.actions;
 export default countriesSlice.reducer;
