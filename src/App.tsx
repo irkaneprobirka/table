7e35054d3547b2f91d7ca1a283ade3e8f82f92de
irkaneprobirka <liptonhwange@gmail.com>
2024-11-19 20:06:51 +0300

    init column in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-// App.tsx
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import ColumnSelector from './components/ColumnSelector';
-import CountriesTable from './components/CountriesTable';
-import { getCountries } from './store/countriesStore/countriesApi';
 import { AppDispatch } from './store';
+import { getCountries } from './store/countriesStore/countriesApi';
+import CountriesTable from './components/CountriesTable';
+import ColumnSelector from './components/ColumnSelector';
+
+const columns = [
+  { id: 'index', title: '#', width: '60px', visible: true },
+  {
+    id: 'name',
+    title: 'Name',
+    visible: true,
+    isWide: true,
+    cellColor: 'lightblue',
+    emptyCellColor: 'lightgray',
+  },
+  {
+    id: 'languages',
+    title: 'Languages',
+    visible: true,
+    isWide: false,
+    cellColor: 'lightyellow',
+    emptyCellColor: 'lightpink',
+  },
+  { id: 'region', title: 'Region', visible: true, isWide: false, emptyCellColor: 'lightpink', },
+  {
+    id: 'population',
+    title: 'Population',
+    visible: false,
+    isWide: false,
+    cellColor: 'lightgreen',
+  },
+  { id: 'status', title: 'Status', visible: true },
+  { id: 'startOfWeek', title: 'Start of Week', visible: true },
+];
 
 const App: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -15,8 +44,8 @@ const App: React.FC = () => {
 
   return (
     <div className="container">
-      <ColumnSelector />
-      <CountriesTable />
+      <ColumnSelector columns={columns} />
+      <CountriesTable columns={columns} />
     </div>
   );
 };
