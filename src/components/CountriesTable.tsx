a7c82141c7beecd5286b4cffb3e25f132afff49a
irkaneprobirka <liptonhwange@gmail.com>
2024-11-19 20:15:52 +0300

    rewrite structure of all table, because we add column with some properties

--- src/components/CountriesTable.tsx
+++ src/components/CountriesTable.tsx
@@ -1,75 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 import '../styles/countriesTable.scss';
+import { CountriesTableProps } from '../types/types';
 
-const CountriesTable: React.FC = () => {
+const CountriesTable: React.FC<CountriesTableProps> = ({ columns }) => {
   const { countries, activeColumns } = useSelector(
     (state: RootState) => state.countries,
   );
-//вычмсляем максимальное значение относительно первого столбца
+  const [expandedRows, setExpandedRows] = useState<Set<string>>(new Set());
+
+  const toggleRow = (countryName: string) =>
+    setExpandedRows((prev) => {
+      const newSet = new Set(prev);
+      newSet.has(countryName) ? newSet.delete(countryName) : newSet.add(countryName);
+      return newSet;
+    });
+
+  const visibleColumns = columns.filter(
+    (col) => col.visible && activeColumns.includes(col.id),
+  );
+
   const maxLength = Math.max(
     ...countries.map((country) => country.name.common.length),
   );
 
   const gridStyle = {
-    gridTemplateColumns: `minmax(${maxLength * 5 }px, 1fr) minmax(${maxLength * 5 }px, 1fr) repeat(${activeColumns.length - 2}, 1fr)`,
+    gridTemplateColumns: visibleColumns
+      .map((col) => {
+        if (col.id === 'index') {
+          return '60px';
+        }
+        return col.isWide ? '2fr' : `minmax(${maxLength * 5}px, 1fr)`;
+      })
+      .join(' '),
   };
 
   return (
     <div className="table">
       <div className="table-header" style={gridStyle}>
-        {activeColumns.map((column) => (
-          <div key={column} className="header-cell">
-            {column}
+        {visibleColumns.map((col) => (
+          <div key={col.id} className="header-cell">
+            {col.title}
           </div>
         ))}
       </div>
 
       <div className="table-body">
-        {countries.map((country) => (
-          <React.Fragment key={country.name.common}>
-            <div className="table-row" style={gridStyle}>
-              {activeColumns.includes('name') && (
-                <div className="table-cell">{country.name.common}</div>
-              )}
-              {activeColumns.includes('languages') && (
-                <div className="table-cell">
-                  {Object.keys(country.languages).length > 1
-                    ? `${Object.keys(country.languages).length} языка(ов)`
-                    : Object.values(country.languages)}
-                </div>
-              )}
-              {activeColumns.includes('region') && (
-                <div className="table-cell">{country.region}</div>
-              )}
-              {activeColumns.includes('population') && (
-                <div className="table-cell">{country.population}</div>
-              )}
-              {activeColumns.includes('status') && (
-                <div className="table-cell">{country.status}</div>
-              )}
-              {activeColumns.includes('startOfWeek') && (
-                <div className="table-cell">{country.startOfWeek}</div>
-              )}
-            </div>
+        {countries.map((country) => {
+          const languages = country.languages ? Object.keys(country.languages) : [];
+          const canExpand = languages.length > 1;
 
-            {activeColumns.includes('languages') &&
-              Object.keys(country.languages).length > 1 &&
-              Object.values(country.languages).map((language, index) => (
-                <div key={index} className="table-row" style={gridStyle}>
-                  <div className="table-cell"></div>
-                  <div className="table-cell">{language}</div>
-                  {/* создаем массив, из длинны которого вычитается -2, так как первая ячейка пустая, а вторая содержит в себе язык */}
-                  {Array.from({ length: activeColumns.length - 2 }).map(
-                    (_, idx) => (
-                      <div key={idx} className="table-cell"></div>
-                    ),
-                  )}
+          const countryRow = (
+            <div
+              className={`table-row ${expandedRows.has(country.name.common) ? 'expanded' : ''}`}
+              style={gridStyle}
+              key={country.name.common}
+            >
+              {visibleColumns.map((col) => {
+                const cellContent = (() => {
+                  switch (col.id) {
+                    case 'index':
+                      return countries.indexOf(country) + 1;
+                    case 'name':
+                      return country.name.common;
+                    case 'languages':
+                      return (
+                        <div
+                          className={`languages-cell ${canExpand ? 'can-expand' : ''}`}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            if (canExpand) toggleRow(country.name.common);
+                          }}
+                        >
+                          {canExpand && (
+                            <span
+                              className={`arrow ${expandedRows.has(country.name.common) ? 'expanded' : ''}`}
+                            ></span>
+                          )}
+                          {canExpand ? `${languages.length} языка(ов)` : languages.join(', ')}
+                        </div>
+                      );
+                    case 'region':
+                      return country.region;
+                    case 'population':
+                      return country.population;
+                    case 'status':
+                      return country.status;
+                    case 'startOfWeek':
+                      return country.startOfWeek;
+                    default:
+                      return '';
+                  }
+                })();
+
+                return (
+                  <div
+                    key={col.id}
+                    className="table-cell"
+                    style={{ backgroundColor: col.cellColor }}
+                  >
+                    {cellContent}
+                  </div>
+                );
+              })}
+            </div>
+          );
+
+          const languageRows = canExpand
+            ? languages.map((language, index) => (
+                <div
+                  className={`table-row language-row ${expandedRows.has(country.name.common) ? 'expanded' : ''}`}
+                  style={gridStyle}
+                  key={`${country.name.common}-lang-${index}`}
+                >
+                  {visibleColumns.map((col) => (
+                    <div
+                      key={col.id}
+                      className={`table-cell ${col.id === 'languages' ? '' : 'empty'}`}
+                      style={{
+                        backgroundColor:
+                          col.id === 'languages' ? col.cellColor || 'transparent' : col.emptyCellColor || 'transparent',
+                      }}
+                    >
+                      {col.id === 'languages' ? language : ''}
+                    </div>
+                  ))}
                 </div>
-              ))}
-          </React.Fragment>
-        ))}
+              ))
+            : [];
+
+          return (
+            <div key={country.name.common}>
+              {countryRow}
+              {languageRows}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
